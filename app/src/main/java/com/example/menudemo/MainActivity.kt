4d6fccbe04b3d3c2a630c5a7cb6c09690290dacc
Ivan Petr <ivan.kiel@gmail.com>
2022-03-29 12:21:08 +0800

    menu added

--- app/src/main/java/com/example/menudemo/MainActivity.kt
+++ app/src/main/java/com/example/menudemo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.menudemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setTitle(R.string.app_name)
+        toolbar.inflateMenu(R.menu.cities)
     }
 }
