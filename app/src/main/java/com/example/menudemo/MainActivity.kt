aa022a6639696115e8fb5a758902087952453464
Ivan Petr <ivan.kiel@gmail.com>
2022-03-29 14:33:34 +0800

    en locale added

--- app/src/main/java/com/example/menudemo/MainActivity.kt
+++ app/src/main/java/com/example/menudemo/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.menudemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cities = resources.getStringArray(R.array.cities)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.setTitle(R.string.app_name)
         toolbar.inflateMenu(R.menu.cities)
+        toolbar.setOnMenuItemClickListener { item -> Log.d("mytag","item: $item"); true }
     }
 }
