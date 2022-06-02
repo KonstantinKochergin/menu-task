5d3f14a7525bc35c28fb0705f84809eea0fb4d5a
Валерия Чупанова <kostyone1@gmail.com>
2022-06-02 22:12:33 +0800

    добавлено меню переключение языков; реализовано переключение языка

--- app/src/main/java/com/example/menudemo/MainActivity.kt
+++ app/src/main/java/com/example/menudemo/MainActivity.kt
@@ -1,18 +1,35 @@
 package com.example.menudemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.os.LocaleList
 import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val cities = resources.getStringArray(R.array.cities)
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         toolbar.setTitle(R.string.app_name)
-        toolbar.inflateMenu(R.menu.cities)
-        toolbar.setOnMenuItemClickListener { item -> Log.d("mytag","item: $item"); true }
+        toolbar.inflateMenu(R.menu.languages)
+        toolbar.setOnMenuItemClickListener {
+            var locale = Locale("ru")
+            if (it.title == "English") {
+                locale = Locale("en")
+            }
+            Locale.setDefault(locale)
+            val config = baseContext.resources.configuration
+            config.locale = locale
+            baseContext.resources.updateConfiguration(config, baseContext.resources.displayMetrics)
+            finish()
+            startActivity(intent)
+            true
+        }
     }
+
 }
